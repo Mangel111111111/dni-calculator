d9f20ae6e6543468af8c346c05836c484985e942
Mangel111111111 <mangel111111111@gmail.com>
2025-01-24 14:10:12 +0100

    feat: add tests

--- app/Http/Controllers/DniController.php
+++ app/Http/Controllers/DniController.php
@@ -31,7 +31,7 @@ class DniController extends Controller
         
         if (!$dniLetter) {
             return response()->json([
-                'message' => 'No se pudo calcular la letra del DNI.',
+                'message' => 'No se pudo calcular la letra del DNI',
             ], 404);
         }
     }
@@ -39,48 +39,48 @@ class DniController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /* public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    /* public function store(Request $request)
     {
         //
-    }
+    } */
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    /* public function show(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    /* public function edit(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    /* public function update(Request $request, string $id)
     {
         //
-    }
+    } */
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    /* public function destroy(string $id)
     {
         //
-    }
+    } */
 }
--- tests/Feature/DniControllerTest.php
+++ tests/Feature/DniControllerTest.php
@@ -2,19 +2,51 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use DB;
 use Tests\TestCase;
+use Database\Seeders\DniLettersSeeder;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DniControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(DniLettersSeeder::class);
     }
+
+    public function test_calculateDniWithValidNumber()
+    {
+        $response = $this->postJson('/api/calculate-dni', [
+            'dniNumber' => 12345678,
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'dniNumber' => 12345678,
+                     'dniLetter' => 'Z',
+                 ]);
+    }
+
+    public function test_returns_404_when_dni_letter_not_found()
+    {
+        DB::table('dni_letters')->truncate();
+
+        $response = $this->postJson('/api/calculate-dni', [
+            'dniNumber' => 12345678,
+        ]);
+
+        $response->assertStatus(404);
+        $response->assertJson([
+            'message' => 'No se pudo calcular la letra del DNI',
+        ]);
+    }
+
 }
